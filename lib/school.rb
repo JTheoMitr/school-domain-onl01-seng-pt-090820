d7dad08a41b93b33d5c487cf0db34dd33b28a788
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-27 19:40:36 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(new_student, grade)
+    roster[grade] ||=[]
+    roster[grade] << new_student
+
+  end
+  
+  def grade(level)
+    roster.detect do |x, y|
+      #here x is key and y is value!
+      if x == level
+        return y
+      end
+    end
+  end
+  
+  def sort 
+  sorted_hash = {}
+  roster.each do |x, y| 
+    sorted_hash[x] = y.sort 
+  end 
+  sorted_hash
+end 
+
+  
+end
